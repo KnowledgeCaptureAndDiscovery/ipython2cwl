c8e726d671e60ffb6eb0e6eb98144bcd8314d6a5
Maximiliano Osorio <maxiosorio@gmail.com>
2021-05-07 14:59:00 -0400

    fix: logging notebook

--- ipython2cwl/repo2cwl.py
+++ ipython2cwl/repo2cwl.py
@@ -146,7 +146,8 @@ def _repo2cwl(git_directory_path: Repo) -> Tuple[str, List[Dict]]:
     shutil.copytree(r2d.repo, bin_path)
 
     notebooks_paths = _get_notebook_paths_from_dir(r2d.repo)
-
+    logging.info(notebook_paths)
+    
     tools = []
     for notebook in notebooks_paths:
         cwl_command_line_tool, script_name = _store_jn_as_script(
