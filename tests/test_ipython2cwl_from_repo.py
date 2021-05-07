d4ddb9be2dc334c4771221de9aa6ccc69905009c
Maximiliano Osorio <maxiosorio@gmail.com>
2021-05-07 11:05:51 -0400

    fix: add new function to test a new notebook

--- tests/test_ipython2cwl_from_repo.py
+++ tests/test_ipython2cwl_from_repo.py
@@ -142,3 +142,107 @@ class Test2CWLFromRepo(TestCase):
         self.assertListEqual(base_commands, ['/app/cwl/bin/simple', '/app/cwl/bin/subdir/simple'])
         script = docker_client.containers.run(dockerfile_image_id, '/app/cwl/bin/subdir/simple', entrypoint='/bin/cat')
         self.assertIn('fig.figure.savefig(after_transform_data)', script.decode())
+
+    def test_notebook_with_imports(self):
+        # setup a simple git repo
+        git_dir = tempfile.mkdtemp()
+        jn_repo = Repo.init(git_dir)
+        shutil.copy(
+            os.path.join(self.here, 'simple_import.ipynb'),
+            os.path.join(git_dir, 'simple_import.ipynb'),
+        )
+        with open(os.path.join(git_dir, 'requirements.txt'), 'w') as f:
+            f.write('pandas\n')
+            f.write('matplotlib\n')
+        jn_repo.index.add('simple_import.ipynb')
+        jn_repo.index.add('requirements.txt')
+        jn_repo.index.commit("initial commit")
+
+        print(git_dir)
+
+        dockerfile_image_id, cwl_tool = _repo2cwl(jn_repo)
+        self.assertEqual(1, len(cwl_tool))
+        docker_client = docker.from_env()
+        script = docker_client.containers.run(dockerfile_image_id, '/app/cwl/bin/simple_import', entrypoint='/bin/cat')
+        self.assertIn('fig.figure.savefig(after_transform_data)', script.decode())
+        messages_array_arg_line = ast.parse(
+            [line.strip() for line in script.decode().splitlines() if '--messages' in line][-1]
+        )
+        self.assertEqual(
+            '+',  # nargs = '+'
+            [k.value.s for k in messages_array_arg_line.body[0].value.keywords if k.arg == 'nargs'][0]
+        )
+        self.assertEqual(
+            'str',  # type = 'str'
+            [k.value.id for k in messages_array_arg_line.body[0].value.keywords if k.arg == 'type'][0]
+        )
+
+        script_tree = ast.parse(script.decode())
+        optional_expression = [x for x in script_tree.body[-1].body if
+                               isinstance(x, ast.Expr) and isinstance(x.value, ast.Call) and len(x.value.args) > 0 and
+                               x.value.args[0].s == '--optional_message'][0]
+        self.assertEqual(
+            False,
+            [k.value for k in optional_expression.value.keywords if k.arg == 'required'][0].value
+        )
+        self.assertEqual(
+            None,
+            [k.value for k in optional_expression.value.keywords if k.arg == 'default'][0].value
+        )
+        self.assertDictEqual(
+            {
+                'cwlVersion': "v1.1",
+                'class': 'CommandLineTool',
+                'baseCommand': '/app/cwl/bin/simple_import',
+                'hints': {
+                    'DockerRequirement': {'dockerImageId': dockerfile_image_id}
+                },
+                'arguments': ['--'],
+                'inputs': {
+                    'dataset': {
+                        'type': 'File',
+                        'inputBinding': {
+                            'prefix': '--dataset'
+                        }
+                    },
+                    'messages': {
+                        'type': 'string[]',
+                        'inputBinding': {
+                            'prefix': '--messages'
+                        }
+                    },
+                    'optional_message': {
+                        'type': 'string?',
+                        'inputBinding': {
+                            'prefix': '--optional_message'
+                        }
+                    }
+                },
+                'outputs': {
+                    'original_image': {
+                        'type': 'File',
+                        'outputBinding': {
+                            'glob': 'original_data.png'
+                        }
+                    },
+                    'after_transform_data': {
+                        'type': 'File',
+                        'outputBinding': {
+                            'glob': 'new_data.png'
+                        }
+                    }
+                },
+            },
+            cwl_tool[0]
+        )
+        cwl = StringIO()
+        yaml.safe_dump(cwl_tool[0], cwl)
+        cwl_code = cwl.getvalue()
+        print(cwl_code)
+
+        #test
+        dockerfile_image_id, new_cwl_tool = _repo2cwl(jn_repo)
+        self.assertEqual(1, len(new_cwl_tool))
+        cwl_tool[0]['hints']['DockerRequirement'].pop('dockerImageId')
+        new_cwl_tool[0]['hints']['DockerRequirement'].pop('dockerImageId')
+        self.assertListEqual(cwl_tool, new_cwl_tool)
